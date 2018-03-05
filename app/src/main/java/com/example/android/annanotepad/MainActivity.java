c825b4eb7b0d26c25b515dfaf9b6d4c38e56a210
ragnar321 <no1@home.com>
2018-03-05 16:40:15 +0000

    Barebones

--- app/src/main/java/com/example/android/annanotepad/MainActivity.java
+++ app/src/main/java/com/example/android/annanotepad/MainActivity.java
@@ -7,10 +7,6 @@ import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
-//    SharedPreferences sharedPref;
-//    SharedPreferences.Editor editor;
-//    EditText todoEditForm;
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -24,23 +20,4 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.action_select_all:
-//                todoEditForm.selectAll();
-//                return true;
-//
-//            case R.id.action_clear_all:
-//                todoEditForm.setText("");
-//                return true;
-//
-//            default:
-//                // If we got here, the user's action was not recognized.
-//                // Invoke the superclass to handle it.
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
 }
